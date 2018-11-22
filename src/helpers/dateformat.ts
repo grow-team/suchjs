cdf404102fe03f95e82012026b4c906674f12249
ganmin <ganmin@360.cn>
2018-11-22 22:04:14 +0800

    add date mockit

--- src/helpers/dateformat.ts
+++ src/helpers/dateformat.ts
@@ -1,4 +1,4 @@
-import { PrototypeMethodNames } from '../types';
+import { NormalObject, PrototypeMethodNames } from '../types';
 import { capitalize } from './utils';
 /*1.形如2016/06/01,2016-06-01,2016.06.01
 2.特殊日期
@@ -164,4 +164,117 @@ const strToDate = (dateStr: string, baseDate?: Date | string | number): Date | n
   } else {
     throw new Error('can not parse the date!');
   }
+};
+export const strtotime = (date: any) => {
+  if(!isNaN(date)) {
+    return new Date(+date);
+  } else if(typeof date === 'string') {
+    let result: Date;
+    try {
+      result = new Date(date);
+    } catch(e) {
+      try {
+        result = strToDate(date);
+      } catch(e) {
+        throw e;
+      }
+    }
+    return result;
+  } else {
+    throw new Error(`invalid date:${date}`);
+  }
+};
+const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+// tslint:disable-next-line:max-line-length
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const formatter: NormalObject = {
+  d() {
+    return this.getDate();
+  },
+  dd() {
+    return zerofill('d', this);
+  },
+  dddd() {
+    return dayNames[this.getDay()];
+  },
+  ddd() {
+    return formatter.dddd.call(this).slice(0, 3);
+  },
+  m() {
+    return this.getMonth() + 1;
+  },
+  mm() {
+    return zerofill('m', this);
+  },
+  mmmm() {
+    return monthNames[this.getMonth()];
+  },
+  mmm() {
+    return formatter.mmmm.call(this).slice(0, 3);
+  },
+  yyyy() {
+    return this.getFullYear().toString().padStart(4, '0');
+  },
+  yy() {
+    return this.getFullYear().toString().slice(-2).padStart(2, '0');
+  },
+  h() {
+    return this.getHours() % 12;
+  },
+  hh() {
+    return zerofill('h', this);
+  },
+  H() {
+    return this.getHours();
+  },
+  HH() {
+    return zerofill('H', this);
+  },
+  M() {
+    return this.getMinutes();
+  },
+  MM() {
+    return zerofill('M', this);
+  },
+  s() {
+    return this.getSeconds();
+  },
+  ss() {
+    return zerofill('s', this);
+  },
+  l() {
+    return this.getMilliseconds().toString().padStart(3, '0');
+  },
+  L() {
+    return Math.round(this.getMilliseconds() / 10).toString().padStart(2, '0');
+  },
+  tt() {
+    return this.getHours() >= 12 ? 'pm' : 'am';
+  },
+  t() {
+    return formatter.tt.call(this).charAt(0);
+  },
+  TT() {
+    return this.getHours() >= 12 ? 'PM' : 'AM';
+  },
+  T() {
+    return formatter.TT.call(this).charAt(0);
+  },
+  S() {
+    return ['st', 'nd', 'rd'][this.getDate() % 10 - 1] || 'th';
+  },
+  N() {
+    return this.getDay() || 7;
+  },
+};
+const zerofill = function(fnName: string, date: Date) {
+  return formatter[fnName].call(date).toString().padStart(2, '0');
+};
+export const dateformat = (fmt: string, date: Date) => {
+  return fmt.replace(/[A-Za-z]*/g, (type: string) => {
+    if(formatter[type]) {
+      return formatter[type].call(date);
+    }
+    return type;
+  });
 };
